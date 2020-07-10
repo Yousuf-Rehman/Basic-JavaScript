3a0b52b4e27e85f79bf30f75ebeefa693e6eda0e
Yousuf Rehman <yousufrehman543@gmail.com>
2020-07-10 14:12:24 +0500

    Removed Recursion from Json Parsing In Folder 3
    3-LoadFlagByUsingAPI script.js file has been
    changed

--- 3-LoadFloagByUsingAPi/script.js
+++ 3-LoadFloagByUsingAPi/script.js
@@ -1,42 +1,9 @@
 'use strict'
 
 document.getElementById('SearchBar').addEventListener("change", onChange);
-var stringConstructor = "".constructor;
-var arrayConstructor = [].constructor;
-var objectConstructor = ({}).constructor;
-let SEARCHKEY = 'flag'
 
 let URL = country => `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
 let upDateDOM_Img = src => document.getElementById("img").src = src
-let FindKeyInJSON = (response, LookUpkey) => {
-  console.log(response)
-  console.log(typeof response)
-  
-  if (response === null) 
-    return "null";
-  
-  else if (response === undefined)
-    return "undefined";
-  
-  else if(response.constructor === stringConstructor)
-      return "string";
-
-  else if(response.constructor === objectConstructor){
-    for(let key in response)
-      if(key === SEARCHKEY){
-        upDateDOM_Img(response[key]);
-        return;
-      }
-      else
-        FindKeyInJSON(response[key], key)
-  }
-  
-  else if(response.constructor === arrayConstructor){
-    for(let each in response){
-      FindKeyInJSON(response[each], each)
-    }
-  }
-}
 
 function onChange() {
   upDateDOM_Img('')
@@ -44,8 +11,14 @@ function onChange() {
   let input_text = document.getElementById('SearchBar').value;
 
   xhr.onload = function() {
-    if(this.status == 200)
-      FindKeyInJSON(JSON.parse(this.responseText), '');
+    if(this.status == 200){
+      let response = JSON.parse(xhr.responseText)
+      try{
+        upDateDOM_Img(response['0']['flag'])
+      }catch(err) {
+        console.error('Could Not parse Data Received')
+      }
+    }
   }
 
   xhr.open('GET', URL(input_text), true)
